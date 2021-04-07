cacee49fe83f2d09710a07ef6a4c772c69c6e3b8
Pabitra <52494098+swainpabitra@users.noreply.github.com>
2021-04-07 17:15:36 +0530

    Commented Summary

--- JWTEncoder.cs
+++ JWTEncoder.cs
@@ -6,34 +6,33 @@ using JWT.Algorithms;
 using JWT.Serializers;
 using Newtonsoft.Json;
 
-namespace Encoders
-{
-    class JWTEncoder
-    {
+namespace Encoders {
+    class JWTEncoder {
         string payload = "{ iat: 1613976225, jti: 'g9f4nchf-98da-a768-5e15-ac71dy934e87', first_name: 'abcdef', last_name: 'xyz', email: 'abc.xyz@abc.com' }";
         
-        public string GetHs256EncoderToken()
-        {
+        ///Get JWT Encoder Token for HS256 algorithm.
+        public string GetHs256EncoderToken(){
             IDictionary<string, object> payloadData = JsonConvert.DeserializeObject<Dictionary<string, object>>(payload);
             IJwtEncoder jwtEncoder = new JwtEncoder(new HMACSHA256Algorithm(), new JsonNetSerializer(), new JwtBase64UrlEncoder());
             return jwtEncoder.Encode(payloadData, "<yourkey>");
         }
-        public string GetRs256EncoderToken(string serialNumber)
-        {
+        
+        ///Get JWT Encoder Token for RS256 algorithm based on Certificate Serial Number
+        public string GetRs256EncoderToken(string serialNumber) {
             X509Certificate2Collection certificates;
             if (serialNumber == null) {
                 throw new ArgumentNullException(nameof(serialNumber));
             }
             IDictionary<string, object> payloadData = JsonConvert.DeserializeObject<Dictionary<string, object>>(payload);
-            
-            using (var store = new X509Store(StoreLocation.LocalMachine)) {//you can change the store location
+            //you can change the store location
+            using (var store = new X509Store(StoreLocation.LocalMachine)) {
                 store.Open(OpenFlags.OpenExistingOnly);
                 certificates = store.Certificates.Find(X509FindType.FindBySerialNumber, serialNumber, true);
             }
             if (certificates.Count == 0)
                 throw new Exception($"Certificate not found based on {serialNumber}");
             IJwtEncoder jwtEncoder = new JwtEncoder(new RS256Algorithm(certificates[0]), new JsonNetSerializer(), new JwtBase64UrlEncoder());
-            return jwtEncoder.Encode(payloadData,new byte[0]);
+            return jwtEncoder.Encode(payloadData, new byte[0]);
         }
     }
 }
